9b2846b6ae1aaa155fb4888c97d7361353bd25b3
Mathieu Lecarme <mlecarme@bearstech.com>
2013-06-25 00:15:38 +0200

    Reading date.

--- poteau/apache.py
+++ poteau/apache.py
@@ -9,11 +9,24 @@ from ua_parser import user_agent_parser
 
 
 RE_COMMON = re.compile(r'(.*?) - (.*?) \[(.*?) [+-]\d{4}\] "(.*?) (.*?) (.*?)" (\d{3}) (\d+)', re.U)
-RE_COMBINED = re.compile('(.*?) - (.*?) \[(.*?) [+-]\d{4}\] "(.*?) (.*?) (.*?)" (\d{3}) (.*?) "([^"]*)" "([^"]*)"', re.U)
-
+RE_COMBINED = re.compile(r'(.*?) - (.*?) \[(.*?) [+-]\d{4}\] "(.*?) (.*?) (.*?)" (\d{3}) (.*?) "([^"]*)" "([^"]*)"', re.U)
+RE_DATE = re.compile(r'(\d+)/(\w{3})/(\d+):(\d+:\d+:\d+)', re.U)
 
 def parse_date(src):
-    pass
+    """
+10/Oct/2000:13:55:36
+2009-11-15T14:12:12
+    """
+    m = RE_DATE.match(src)
+    MONTH = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+             'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    resp = '%(year)s-%(month)02d-%(day)02dT%(hms)s' % {
+        'year': m.group(3),
+        'month': MONTH.index(m.group(2)) + 1,
+        'day': int(m.group(1)),
+        'hms': m.group(4)
+        }
+    return resp
 
 
 def intOrZero(a):
