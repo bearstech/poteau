de03f11c780af954e2e7aae0c1cda92d1dd9b2c6
Mathieu Lecarme <mlecarme@bearstech.com>
2013-07-01 22:12:45 +0200

    Setup stuffs.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='poteau',
+      version='0.1',
+      author='Mathieu Lecarme',
+      author_email='mlecarme@bearstech.com',
+      license='BSD',
+      packages=['poteau'],
+      scripts=['scripts/poteau'],
+      install_requires=['pyyaml',
+                'ua-parser',
+                'lamson',
+                'pyelasticsearch',
+                'docopt >=0.6'
+                ]
+      )
