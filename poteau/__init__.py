6fe13a17cf6ba9d42a180d59a5671139e441aca1
Mathieu Lecarme <mlecarme@bearstech.com>
2013-07-05 18:47:32 +0200

    Bulk can mix index.

--- poteau/__init__.py
+++ poteau/__init__.py
@@ -1,5 +1,3 @@
-from pyelasticsearch.exceptions import ElasticHttpNotFoundError
-
 
 def bulk_iterate(collection, bulk_size):
     """Agnostic way for bulk iteration"""
@@ -17,36 +15,39 @@ class Kibana(object):
     def __init__(self, es):
         self.es = es
 
+    def _bulk_action_data(self, index, doc_type, doc):
+        action = {'index': {'_index': index, '_type': doc_type}}
+
+        if doc.get('id') is not None:
+            action['index']['_id'] = doc['id']
+
+        return self.es._encode_json(action), self.es._encode_json(doc)
+
     def index_documents(self, type_, documents):
         """
 https://github.com/logstash/logstash/wiki/logstash%27s-internal-message-format
         """
-        current = None
+        day = None
         stack = []
 
-        def bulk(current, type_, stack):
+        def bulk(day, type_, stack):
             if len(stack):
-                idx = 'logstash-%s' % current.replace('-', '.')
-                self.es.bulk_index(idx, type_, stack)
-                #self.es.flush(idx)
-                return current, len(stack)
+                body = '\n'.join(stack) + '\n'
+
+                self.es.send_request('POST',
+                                 ['_bulk'],
+                                 body,
+                                 encode_body=False)
+                return day, len(stack)
 
         for document in documents:
             day = document['@timestamp'].split('T')[0]
-            if current is None:
-                current = day
-            if current != day:
-                try:
-                    self.es.close_index(current)
-                except ElasticHttpNotFoundError:
-                    pass
-                yield bulk(current, type_, stack)
-                stack = [document]
-                current = day
-            else:
-                stack.append(document)
+            action, data = self._bulk_action_data(
+                    'logstash-%s' % day.replace('-', '.'), type_, document)
+            stack.append(action)
+            stack.append(data)
             if len(stack) >= 300:
-                yield bulk(current, type_, stack)
+                yield bulk(day, type_, stack)
                 stack = []
 
-        yield bulk(current, type_, stack)
+        yield bulk(day, type_, stack)
